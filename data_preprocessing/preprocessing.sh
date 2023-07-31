80e1995efd26979653e35ed3db799ac9ebc70967
hyunjung <hyunjung.kim@fainders.ai>
2023-07-31 18:22:53 +0900

    preprocess

---
+++ data_preprocessing/preprocessing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+python3 ./draw_label.py
+python3 ./rename.py
+python3 ./data_crop.py
+
---
+++ data_preprocessing/rename.py
@@ -0,0 +1,50 @@
+#-*- coding: utf-8 -*-
+
+# import torch, torchvision
+# import mmseg
+# import mmcv
+# import mmengine
+# import matplotlib.pyplot as plt
+import os.path as osp
+# import numpy as np
+import os
+import shutil
+import sys
+
+
+path_pre=os.getcwd()
+path_now=path_pre[:-19]
+#dacon에서 다운로드 후 data폴더 저장 경로
+data_path = path_now+'/open/' ##
+#TRAIN_0000.png -> 0000.png와 같이 rename한 data폴더 저장할 경로
+output_path = path_now+"/mmsegmentation/dataset" ##
+
+img_src = os.path.join(data_path, 'train_img/')
+lab_src = os.path.join(data_path, 'train_label/')
+
+img_output = os.path.join(output_path, 'train_img/')
+lab_output = os.path.join(output_path, 'train_label/')
+
+# 처음에만!!! 
+def renameFiles(data_directory,output_directory, target_label):
+    # Iterate over all files in the source directory
+    for filename in os.listdir(data_directory):
+        if filename.startswith(target_label) and os.path.isfile(os.path.join(data_directory, filename)):
+            # Remove the 'TRAIN_' prefix from the filename
+            new_filename = filename.replace(target_label, '')
+            # Create the destination path with the updated filename
+            source_path = os.path.join(data_directory, filename)
+            destination_path = os.path.join(output_directory, new_filename)
+
+            # Rename the file
+            os.rename(source_path, destination_path)
+
+
+if not os.path.exists(str(path_now)+'/mmsegmentation/dataset/train_img'):
+    os.makedirs(str(path_now)+'/mmsegmentation/dataset/train_img')
+
+if not os.path.exists(str(path_now)+'/mmsegmentation/dataset/train_label'):
+    os.makedirs(str(path_now)+'/mmsegmentation/dataset/train_label')
+
+renameFiles(img_src,img_output, 'TRAIN_') #TRAIN_0000.png -> 0000.png
+renameFiles(lab_src,lab_output, 'LABEL_')
